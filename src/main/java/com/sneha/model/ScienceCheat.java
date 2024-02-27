cc9dff55f09d8870f6b209a91c5b180888fd7f7c
sneha <inbox.sneha.santhosh@gmail.com>
2024-02-27 11:09:38 +0530

    selenium: if i need to use only science cheat in student class..i need to do lot of changes(remove math cheat, add science cheat and remove it's setter method if used...etc)Instead use interface (Cheat)

---
+++ src/main/java/com/sneha/model/ScienceCheat.java
@@ -0,0 +1,8 @@
+package com.sneha.model;
+
+public class ScienceCheat {
+
+    public void scienceCheat(String name) {
+        System.out.println("Science Cheating started by "+name);
+    }
+}
--- src/main/java/com/sneha/model/Student.java
+++ src/main/java/com/sneha/model/Student.java
@@ -2,22 +2,25 @@ package com.sneha.model;
 public class Student {
     private String studentName;
 
-    MathCheat mathCheat;
-
+//    MathCheat mathCheat;   // using the implementation/class reference
+    ScienceCheat scienceCheat;
     public Student() {
         System.out.println("No args Student constructor called");
     }
-    public Student(MathCheat mathCheat) {
-        this.mathCheat = mathCheat;
+    public Student(MathCheat mathCheat, ScienceCheat scienceCheat) {
+//        this.mathCheat = mathCheat;
+        this.scienceCheat = scienceCheat;
     }
 
-    public Student(String studentName, MathCheat mathCheat) {
-        this.mathCheat = mathCheat;
+    public Student(String studentName, MathCheat mathCheat, ScienceCheat scienceCheat) {
+//        this.mathCheat = mathCheat;
+        this.scienceCheat = scienceCheat;
         this.studentName = studentName;
     }
 
     public void studentCheating(String name) {
-        mathCheat.mathCheating(name);
+//        mathCheat.mathCheating(name);
+        scienceCheat.scienceCheat(name);
     }
 
 }
